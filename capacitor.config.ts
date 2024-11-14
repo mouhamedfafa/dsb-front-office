9df677a20f9b74a3880aea11a4ab042499abbb45
Mouhamed Faye <faymouhamed1994@gmail.com>
2024-11-14 12:04:16 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dsb-front-office',
+  webDir: 'www'
+};
+
+export default config;
